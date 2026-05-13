1625b8fbe9b20e8c840a32954eed4a164bd8dcad
Ayoub <ayoub@example.com>
2026-05-13 23:03:19 +0200

    Add dialogue integration between Unity and FastAPI

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 # AI-Talking-NPC
 
-moving the cube left or right
+
+### <span style="color: #4c29b6;">Moving the cube</span>
+## move right or left
 ![moving cube left or right](<images/moving cube left or right.png>)
+
+##
+### <span style="color: #4c29b6;">Testing the API </span>
+
+
+
+#### GET
+![Testing the Api GET endpoint](images/testing_API_GET_Request.png)
+#### POST
+![alt text POST endpoint](images/testing_API_POST_Request.png)
+
+##
+### <span style="color: #4c29b6;"> How it looks like </span>
+
+#### the start / default state 
+![the start state](images/default.png)
+
+#### hello_reponse
+![it is returned when your text contains the "hello" word ](images/hello_reponse.png)
+
+#### Defult_response
+![Defult_response is returned when no match is found](images/Defult_response.png)
+
+#### Connection_Error
+![Connection_Error (for exemple in case of no internet or no API)](images/Connection_Error.png)
--- backend/main.py
+++ backend/main.py
@@ -3,27 +3,31 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-# Request format from Unity
+# Request model from Unity
 class ChatRequest(BaseModel):
     message: str
 
-# API endpoint
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the AI Tutor API!"}
+    return {
+        "message": "AI Tutor API Running"
+    }
 
 @app.post("/chat")
 async def chat(data: ChatRequest):
 
-    user_message = data.message
+    user_message = data.message.lower()
 
-    #  fake response logic based on user message
+    # Simple AI dialogue logic
 
-    if "hello" in user_message.lower():
+    if "hello" in user_message:
         answer = "Hello student!"
-    
-    elif "language" in user_message.lower():
-        answer = "VR can improve language learning."
+
+    elif "language" in user_message:
+        answer = "VR improves immersive language learning."
+
+    elif "unity" in user_message:
+        answer = "Unity is useful for VR simulations."
 
     else:
         answer = "I am your AI tutor."
